f79a1af5c764c9e75b2bcd9321ee9423dd28869b
Jere Leman <56977360+JereLem@users.noreply.github.com>
2024-07-26 19:50:46 +0300

    Update DualIBT2MotorShield.h

--- DualIBT2MotorShield.h
+++ DualIBT2MotorShield.h
@@ -21,15 +21,7 @@ class DualIBT2MotorShield
     void setSpeeds(int m1Speed, int m2Speed); // Set speed for both M1 and M2.
 
   private:
-    unsigned char _RPWM1;
-    unsigned char _LPWM1;
-    static const unsigned char _PWM1_TIMER1_PIN = 9;
+    // Pin mapping
+    unsigned char _RPWM1, _LPWM1, _RPWM2, _LPWM2;
 
-    unsigned char _RPWM2;
-    unsigned char _LPWM2;
-    static const unsigned char _PWM2_TIMER2_PIN = 10;
-
-
-    void initTimer1For20kHz();
-    void initTimer2For20kHz();
 };
